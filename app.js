7da8c3de11af90477cf90160cccb3a562188f9ac
Dāvis Šķipsna <skipsna.davis@gmail.com>
2020-09-13 06:04:44 -0700

    deploy on heroku

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ app.set("view engine", "pug");
  */
 app.use(routes);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`App is listening on port: ${port}`));
